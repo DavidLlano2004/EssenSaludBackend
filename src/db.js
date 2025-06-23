312c0a8cd6998251212b562f78916a59bc45462e
DavidLlano2004 <juli.matias.2004@gmail.com>
2025-06-22 20:57:07 -0500

    Se suben cambios de base de datos

--- src/db.js
+++ src/db.js
@@ -4,20 +4,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  "proyecto_web", //Nombre base de datos
-  "proyecto_web", // Usuario
-  "admin1234",
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+// export const sequelize = new Sequelize(
+//   "proyecto_web", //Nombre base de datos
+//   "proyecto_web", // Usuario
+//   "admin1234",
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: 3306,
+//   }
+// );
 
-// export const sequelize = new Sequelize(process.env.DB_URL, {
-//   // host: "localhost",
-//   dialect: "mysql",
-//   // port: process.env.DB_PORT || 3306, // opcional
-//   logging: false,
-// });
+export const sequelize = new Sequelize(process.env.DB_URL, {
+  // host: "localhost",
+  dialect: "mysql",
+  // port: process.env.DB_PORT || 3306, // opcional
+  logging: false,
+});
